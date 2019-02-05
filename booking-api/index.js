879146320046209727a7773e1f45a423f6a0840d
punya <punyachapagain@gmail.com>
2019-02-05 19:15:06 +1100

    create admin routes

--- booking-api/index.js
+++ booking-api/index.js
@@ -1,30 +1,15 @@
 const express = require("express");
 const app = express();
-const Config = require("./config/config.json");
+const Config = require('./config/config.json');
 const bodyParser = require('body-parser');
 const port = Config.port || 4000;
 const morgan = require('morgan');
-
+const adminRouter = require('./routes/adminRouter');
 app.use(morgan('dev'))
 app.use(bodyParser.json());
 
 
-app.all("/", function (req, res, next) {
 
+app.use('/admin', adminRouter)
 
-  console.log("Accessing the secret section ...");
-
-
-  next(); // pass control to the next handler
-});
-app.get("/", function (req, res, next) {
-  console.log(req.header)
-  res.send("hello")
-});
-
-app.post("/", function (req, res, next) {
-
-});
-
-console.log("heloo");
 app.listen(port, console.log("Starting Server in Port " + port));
---
+++ booking-api/routes/adminRouter.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const adminRouter = express.Router();
+
+adminRouter.route('/')
+    // .all((req, res, next) => {
+    //     res.statusCode = 200;
+    //     res.setHeader('Content-Type', 'text/plain');
+    //     next();
+    // })
+
+    .get((req, res, next) => {
+        res.send("i am sending you the data");
+    })
+
+    .post((req, res, next) => {
+        res.send("data posted")
+    })
+
+
+
+
+
+
+module.exports = adminRouter;
